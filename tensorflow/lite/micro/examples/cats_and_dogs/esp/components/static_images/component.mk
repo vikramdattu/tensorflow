3b9952c57e94845f96b9ebf26bfe345d2a994d90
Vikram Dattu <vikram.dattu@espressif.com>
2020-12-03 20:14:16 +0530

    Added cats_and_dogs example to `micro/examples` directory
    Signed-off-by: Vikram Dattu <vikram.dattu@espressif.com>

---
+++ tensorflow/lite/micro/examples/cats_and_dogs/esp/components/static_images/component.mk
@@ -0,0 +1,15 @@
+#
+# Component Makefile
+#
+
+
+COMPONENT_EMBED_TXTFILES := static_images/image0 \
+                            static_images/image1 \
+                            static_images/image2 \
+                            static_images/image3 \
+                            static_images/image4 \
+                            static_images/image5 \
+                            static_images/image6 \
+                            static_images/image7 \
+                            static_images/image8 \
+                            static_images/image9
